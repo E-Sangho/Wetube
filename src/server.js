f4cd9115aa87a81eb32eb79e0f4f46dc878b49b1
Sangho <istrangeho@gmail.com>
2021-09-05 23:48:22 +0900

    modified middleware

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
 
-app.get("/", handleHome);
+const handleHome = (req, res) => {
+    return res.send("I love middle ware");
+};
+
+app.use(morgan("dev"));
+app.get("/", logger, handleHome);
+
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
